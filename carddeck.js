e09c717fe716e43f3b0020fca1b8be2f6601406f
joshuastjohn234 <84975840+joshuastjohn234@users.noreply.github.com>
2021-06-20 02:18:49 -0400

    done
    changed the game to I declare war

--- carddeck.js
+++ carddeck.js
@@ -6,6 +6,18 @@ const Value =[ 'A','2','3','4','5','6','7','8','9','10','J','Q','K'];
          this.cards = cards
          
      }  
+     get numberOfCards(){
+       return this.cards.length
+     }
+    
+    pop() {
+      return this.cards.shift()
+    }
+    
+    push(card) {
+    this.cards.push(card)
+    }
+
   shuffle() {
     this.cards.sort((a, b)=> Math.random() - .5)
     }  
@@ -16,16 +28,18 @@ const Value =[ 'A','2','3','4','5','6','7','8','9','10','J','Q','K'];
           this.value = value
           
    }
-   getcolor() {
-     return this.suit === '♠'|| this.suit === '♣' ? 'black' : ' red'
+   get color () {
+     return this.suit ==='♠' || this.suit === '♣' ? 'black' : 'red'
    }
-  getHTML(){
-  const cardDiv = document.createElement('div')
-  cardDiv.innerText = this.suit
-  cardDiv.classList.add("card", this.color)
-  cardDiv.dataset.value = `${this.value} ${this.suit}`
-  return cardDiv
- }
+
+    getHTML() {
+     const cardDiv= document.createElement('div');
+     cardDiv.innerText = this.suit;
+     cardDiv.classList.add("card", this.color);
+     cardDiv.dataset.value = `${this.value} ${this.suit}`
+     return cardDiv
+     
+   }  
 }  
 
 function playDeck() {
